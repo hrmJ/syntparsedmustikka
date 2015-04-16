a9cd77259daaefc1869bee8ad115d7104dbf3835
Juho Härme <juho.harme@uta.fi>
2015-04-16 14:09:54 +0300

    First version of valency chain creator

--- phdqueries.py
+++ phdqueries.py
@@ -18,10 +18,6 @@ class Csvlist:
         with open(csvpath, 'r') as f:
             self.aslist = list(csv.reader(f))
 
-def joinidlist(idrows):
-    idlist =  list(itertools.chain(*idrows))
-    idlist = ','.join(map(str, idlist)) 
-    return idlist
 
 class FinnishTime:
 
@@ -57,22 +53,102 @@ class FinnishTime:
         self.subq = FinnishTimeWordsSubQ
         self.qwords = qwords
 
-def BuildGroupString(match,attribute):
+def BuildGroupString(match,attribute,accepted_dependencies=list()):
     """This methods constructs strings that 
     can be used in statistical analysis. The created strings represent 
     the nuclei that are dependent on the same head as the matched nucleus"""
     groupmembers = dict()
     for tokenid, word in match.matchedsentence.words.items():
+        #Mark the head with square brackets
+        if tokenid == match.matchedword.head:
+            groupmembers[tokenid] = "[{}]".format(getattr(word,attribute))
         # Pick all the word objects that have the same head and that are not punctuation marks
         if word.head == match.matchedword.head and word.token not in string.punctuation:
             if tokenid == match.matchedword.tokenid:
                 groupmembers[tokenid] = "<{}>".format(getattr(word,attribute))
             else:
-                groupmembers[tokenid] = getattr(word,attribute)
+                #if a list of accepted deprelations is defined, take into account only those
+                if accepted_dependencies:
+                    if word.deprel in accepted_dependencies:
+                        groupmembers[tokenid] = getattr(word,attribute)
+                #Don't take into account conjunctions
+                elif word.deprel not in ('cc','conj'):
+                    groupmembers[tokenid] = getattr(word,attribute)
     # Build a string from the attributes
-    for gmkey in sorted(groupmembers.keys()):
+    for gmkey in sorted(map(int,groupmembers)):
         try:
             groupstring += "|" + groupmembers[gmkey]
         except:
             groupstring = groupmembers[gmkey]
     return groupstring
+
+def joinidlist(idrows):
+    """Flatten the lists from database"""
+    idlist =  list(itertools.chain(*idrows))
+    idlist = ','.join(map(str, idlist)) 
+    return idlist
+
+def BuildDataGroupString(db,table):
+    "Define a shortcut string for different data groups"
+    if db=='syntparfin' and table == 'fi_conll':
+        return 'FS'
+    elif db=='syntparfin' and table == 'ru_conll':
+        return 'RT'
+    elif db=='syntparrus' and table == 'fi_conll':
+        return 'FT'
+    elif db=='syntparrus' and table == 'ru_conll':
+        return 'RS'
+    else:
+        return 'unknown'
+
+def DetermineClausesLastWordId(match):
+    """A method that calculates the tokeid of the last word in the matched clause"""
+    sentencekeys =  sorted(map(int,match.matchedsentence.words))
+    #loop starting from the index of the matched word
+    for idx in sentencekeys[int(match.matchedword.tokenid):]:
+        pass
+
+
+def AnalyzeTimeAdvlData1(search):
+    """Annotate material for use in phd"""
+    #Connect to database
+    con = mydatabase('matches','juho')
+    print('Inserting data analysis to database')
+    #Loop through all the matches in a search object
+    i = 0
+    for alignid, matchlist in search.matches.items():
+        for match in matchlist:
+            #Build human readable strings:
+            match.BuildSentencePrintString()
+            #insert values to the metadata table
+            SQL = "INSERT INTO metadata (hlsentence, sentence, sourcetext, datagroup, db, dbtable,matched_sentence_id,matched_word_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+            con.insertquery(SQL,(match.matchedsentence.printstring,
+                                match.matchedsentence.cleanprintstring,
+                                match.sourcetextid,
+                                BuildDataGroupString(search.queried_db,search.queried_table),
+                                search.queried_db,
+                                search.queried_table,
+                                match.matchedsentence.sentence_id,
+                                match.matchedword.dbid
+                                )) 
+
+            #get the id of the inserted metadata item
+            SQL = "SELECT max(id) FROM metadata"
+            linkid = con.nondictquery(SQL)
+            linkid = linkid[0][0]
+            SQL = "INSERT INTO valency_information (linkid, valency_chain, lemma_chain, pos_chain, head_type, narrowed_valency_chain) VALUES (%s,%s,%s,%s,%s,%s)"
+            #Define some special sets of dependency relations
+            deprellimits1 = ('dobj','nommod','advmod')
+            #insert values to the valency_information table
+            con.insertquery(SQL,(linkid,BuildGroupString(match,'deprel'),
+                                BuildGroupString(match,'lemma'),
+                                BuildGroupString(match, 'pos'),
+                                match.matchedsentence.words[match.matchedword.head].deprel,
+                                BuildGroupString(match, 'deprel',deprellimits1)
+                                )) 
+        i += 1
+        print('Analyzed {}/{} matches.'.format(i,len(search.matches.items())), end='\r')
+    print('')
+    print('Analysis completed.')
+    print('')
+
