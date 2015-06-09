b083a0a2836f01ed76667fbc08ce049968992291
Juho Härme <juho.harme@uta.fi>
2015-06-09 11:08:14 +0300

    Getting SN finalized

--- contrastive_layer2.py
+++ contrastive_layer2.py
@@ -1,39 +1,112 @@
 #! /usr/bin/env python
 import logging
 import sys
-from sqlalchemy import create_engine, update, Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dbmodule import SqlaCon, mydatabase, psycopg
+from dbmodule import psycopg, mydatabase
 from search import Search, Match, Sentence, Word, ConstQuery, Db 
-import contrastive_deptypes
+import sn
  
-dbname = 'syntparrus'
-engine = create_engine('postgresql:///{}'.format(dbname), echo=False)
-Base = declarative_base(engine)
+class Featset:
+    """Predefined feature sets and some methods to define them on the fly"""
+    #REMEMBER  PRONONUNS
+    def __init__(self):
+        self.NounAcc =  self.createNounSet(cases = ('a',))
+        self.NounDat =  self.createNounSet(cases = ('d',))
+        self.PronAcc =  self.createPronSet(cases = ('a',))
+        self.PronDat =  self.createPronSet(cases = ('d',))
+        self.inf = ('Vmn----a-e','Vmn----a-p')
+        #Pronouns
 
-class SnData(Base):
-    """Access the SN analyzed data with sqlalchemy"""
-    __tablename__ = 'ru_conll'
-    __table_args__ = {'autoload':True}
 
-def updateidlist(idlist,con):
-    pass
-    #con.insertquery("UPDATE fi_conll SET contr_deprel = NULL")
+    def createNounSet(self,pos = 'N', nountypes = ('c','p'), genders = ('m','f','n'), numbers = ('s','p'), cases = ('n','g','a','i','d','l'), animacies = ('n','y')):
+       """ Create tuples with all the specified feats"""
+       items = self.additemlist([pos],nountypes)
+       items = self.additemlist(items,genders)
+       items = self.additemlist(items,numbers)
+       items = self.additemlist(items,cases)
+       items = self.additemlist(items,animacies)
+       return tuple(items)
+
+    def createPronSet(self,pos = 'P-', person = ('1','2','3','-'), genders = ('m','f','n','-'), numbers = ('s','p','-'), cases = ('n','g','a','i','d','l','-'), postype = ('n','a','r')):
+       """ Create tuples with all the specified feats"""
+        #P-3msin
+       items = self.additemlist([pos],person)
+       items = self.additemlist(items,genders)
+       items = self.additemlist(items,numbers)
+       items = self.additemlist(items,cases)
+       items = self.additemlist(items,postype)
+       return tuple(items)
+
+    def additemlist(self, oldvalues, newvalues):
+        """Create a list with all the combinations of the supplied values"""
+        newitems = list()
+        for oldvalue in oldvalues:
+            for newvalue in newvalues:
+                newitems.append(oldvalue + newvalue)
+        return newitems
+
+def LogNewDeprel(message):
+    """Log the deprels name as header"""
+    logging.info('{0}{1}{0}{2}{0}{1}{0}'.format('\n','='*50,message))
+
+def log(message):
+    """Log just a simple message"""
+    logging.info(message)
+
+def simpleupdate(thisSearch,dbcon, deprel):
+    """In the prototypical case you just give the deprel of the contrastive layer"""
+    logging.info('Updating {} items in the db'.format(len(thisSearch.listMatchids())))
+    dbcon.query('UPDATE ru_conll SET contr_deprel = %(deprel)s WHERE id in %(idlist)s',{'deprel':deprel,'idlist':thisSearch.idlist})
+    logging.info('to be updated: {} database rows.'.format(dbcon.cur.rowcount))
+
+def makeSearch(ConditionColumns,database,dbtable,headcond=None,depcond=None):
+    Db.searched_table = dbtable
+    thisSearch = Search(database,askname=False)
+    logging.info('Starting the search..')
+    thisSearch = Search(database,askname=False)
+    thisSearch.ConditionColumns.append(ConditionColumns)
+    thisSearch.headcond = headcond
+    thisSearch.depcond = depcond
+    thisSearch.BuildSubQuery()
+    thisSearch.find()
+    logging.info('Search committed')
+    return thisSearch
 
 def createContrastiveLayer():
     """Creates the contrastive layer phase by phase"""
     prcon = psycopg('syntparrus','juho')
     pfcon = psycopg('syntparfin','juho')
-    #contrastive_deptypes.sn_obj(prcon)
-    #contrastive_deptypes.sn_gmod_own(prcon)
-    contrastive_deptypes.sn_infcomp_from_predik(prcon)
+    #sn.gdep(prcon)
+    #sn.obj(prcon)
+    #sn.gmod_own(prcon)
+    #sn.infcomp(prcon)
+    #sn.prtcl(prcon)
+    sn.prdctv(prcon)
+    #sn.semsubj(prcon)
+    #sn.cdep(prcon)
+    #sn.conj(prcon)
+    #sn.attr(prcon)
     prcon.connection.commit()
-
 
 #Start:
 if __name__ == "__main__":
 
+    #Initialize a logger
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s: %(message)s')
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('logof_contrastivelayer.txt')
+    fh.setLevel(logging.DEBUG)
+
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    root.addHandler(fh)
+    root.addHandler(ch)
+
     #prcon = mydatabase('syntparrus','juho')
-    #prcon.insertquery("UPDATE ru_conll SET contr_deprel = NULL WHERE contr_deprel IS NOT NULL")
+    #prcon.insertquery("UPDATE ru_conll SET contr_deprel = NULL, contr_head = NULL")
     createContrastiveLayer()
---
+++ sn.py
@@ -0,0 +1,271 @@
+#! /usr/bin/env python
+from contrastive_layer2 import Featset, LogNewDeprel, simpleupdate, makeSearch, log
+
+class Featset:
+    """Predefined feature sets and some methods to define them on the fly"""
+    #REMEMBER  PRONONUNS
+    def __init__(self):
+        self.NounAcc =  self.createNounSet(cases = ('a',))
+        self.NounDat =  self.createNounSet(cases = ('d',))
+        self.PronAcc =  self.createPronSet(cases = ('a',))
+        self.PronDat =  self.createPronSet(cases = ('d',))
+        self.inf = ('Vmn----a-e','Vmn----a-p')
+        #Pronouns
+
+
+    def createNounSet(self,pos = 'N', nountypes = ('c','p'), genders = ('m','f','n'), numbers = ('s','p'), cases = ('n','g','a','i','d','l'), animacies = ('n','y')):
+       """ Create tuples with all the specified feats"""
+       items = self.additemlist([pos],nountypes)
+       items = self.additemlist(items,genders)
+       items = self.additemlist(items,numbers)
+       items = self.additemlist(items,cases)
+       items = self.additemlist(items,animacies)
+       return tuple(items)
+
+    def createPronSet(self,pos = 'P-', person = ('1','2','3','-'), genders = ('m','f','n','-'), numbers = ('s','p','-'), cases = ('n','g','a','i','d','l','-'), postype = ('n','a','r')):
+       """ Create tuples with all the specified feats"""
+        #P-3msin
+       items = self.additemlist([pos],person)
+       items = self.additemlist(items,genders)
+       items = self.additemlist(items,numbers)
+       items = self.additemlist(items,cases)
+       items = self.additemlist(items,postype)
+       return tuple(items)
+
+    def additemlist(self, oldvalues, newvalues):
+        """Create a list with all the combinations of the supplied values"""
+        newitems = list()
+        for oldvalue in oldvalues:
+            for newvalue in newvalues:
+                newitems.append(oldvalue + newvalue)
+        return newitems
+
+def gdep(dbcon=False):
+    """"Aluksi määritellään kaikkien elementtien luokaksi gdep, jota sitten
+    tarkennetaan niin pitkälle kuin mahdollista    """
+
+    LogNewDeprel('Create the category of gdep in the SN data')
+    dbcon.query('UPDATE ru_conll SET contr_head = head, contr_deprel = %(contrdep)s',{'contrdep':'gdep'})
+    log('The gdep category was succesfully created')
+
+def obj(dbcon=False):
+    """" Toiseksi SN-analysoidun aineiston jäsennystä on tarkennettava niin, että
+    siihenkin luodaan oma kategoriansa objekteille. 
+    
+    Teknisesti tämä ei ole erityisen
+    vaikeaa, sillä objekteiksi voitaneen melko aukottomasti määrittää
+    SN-analysoidusta aineistosta ne kompl1-luokkaan sijoitetut sanat, jotka a) ovat
+    verbistä riippuvia nomineja ja b) on morfologisessa analyysissa luokiteltu joko
+    akkusatiivisijaisiksi feminiineiksi/neutreiksi tai genetiivisijaisiksi
+    elollisiksi maskuliineiksi/monikkomuodoiksi. 
+    
+    Luotavaan objektikategoriaan
+    liitetään myös alkuperäisen SN-analyysin *dliteln*-luokka, joka sisältää
+    akkusatiivissa käytettyjä keston ajanilmauksia.  Tämä ratkaisu on yhtenevä sen
+    kanssa, miten TDT-analyysi käsittelee objektinsijaisia määrän adverbiaaleja
+    (tarkemmin ks. ).
+    """
+
+    featset = Featset()
+    LogNewDeprel('Create the category of object in the SN data')
+    thisSearch = makeSearch(database='syntparrus', dbtable='ru_conll', ConditionColumns={'feat':featset.NounAcc,'deprel':('1-компл','2-компл','длительн')}, headcond = {'column':'pos','values':('V',)})
+    simpleupdate(thisSearch, dbcon, deprel='obj')
+
+def gmod_own(dbcon=False):
+    """"Create the category of gmod_own in SN
+    NEEDS ADJUSTMENTS, Something's probably being left out 
+    ------------------------------
+    """
+    featset = Featset()
+    LogNewDeprel('Create the category of gmod_own in the SN data')
+    thisSearch = makeSearch(database='syntparrus',dbtable='ru_conll', ConditionColumns={'token':('у',),'deprel':('1-компл',)}, headcond = {'column':'lemma','values':('быть','есть', 'бывать', 'нет','мало','много')})
+    simpleupdate(thisSearch, dbcon, deprel='gmod-own')
+
+def infcomp(dbcon=False):
+    """Muokkaan kontrastiivista analyysikerrosta varten SN-analyysiä siten, että
+    kaikki infinitiivit, jotka eivät ole lauseen pääsanan asemassa, 
+    luokitellaan infcomp-kategoriaan
+    (mukaanlukien verbillä ilmaistavat subjektit)
+
+    Kuten nesessiivilauseiden osalta, tukeudun myös apuverbien kohdalla SN-analyysin mukaiseen malliin ja
+    määrittelen kontrastiivisessa analyysikerroksessa pääsanaksi apuverbin ja dependentiksi pääverbin. Tästä
+    seuraa, että apuverbin dependenssityyppi on joko ROOT tai jokin sivu- tai rinnasteisen lauseen pääsanan
+    kategoria, jolloin pääverbille on erikseen määriteltävä oma kategoriansa – tällaiseksi soveltuu edellä
+    SN-aineiston infinitiivisubjektien yhteydessä määritelty infcomp-luokka. TDT-analyysin kannalta tämä
+    tarkottaa sitä, että kieltoverbien kantama informaatio lauseen kielteisyydestä katoaa kontrastiivisesta
+    analyysikerroksesta. Myös TDT:n aux-, auxpass- ja SN:n analit-kategoriat poistuvat
+    """
+
+    featset = Featset()
+    LogNewDeprel('Create the infcomp category in SN')
+    thisSearch = makeSearch(database='syntparrus',dbtable='ru_conll',  ConditionColumns={'deprel':('предик','1-компл','2-компл','аналит'),'pos':('V',),'feat':featset.inf})
+    simpleupdate(thisSearch, dbcon, deprel='infcomp')
+
+def prtcl(dbcon=False):
+    """
+    SN-analyysissä kieltopartikkelit on
+    luokiteltu kategoriaan ogranitš, johon kuuluvat myös käytännössä kaikki muut partikkelit konditionaalin
+    12ilmaisevaa by-partikkelia lukuun ottamatta. Ogranitš-luokka siirretään sellaisenaan kontrastiiviseen
+    analyysitasoon ja nimetään uudelleen tunnisteella prtcl. Tähän luokkaan yhdistetään myös alun
+    perin analit-kategoriaan luokitellut by-partikkelit
+
+    Partikkelit SN-datassa voivat olla:
+    предик сент-предик 1-компл неакт-компл колич-огран сент-соч обст предл аппоз релят подч-союзн ROOT разъяснит опред эксплет вспом композ пролепт атриб оп-опред сравнит вводн соч-союзн кратн квазиагент аналит соотнос 2-компл ном-аппоз огранич сочин примыкат 3-компл присвяз сравн-союзн 
+    """
+
+    LogNewDeprel('Create the prtcl category in SN')
+    thisSearch = makeSearch(database='syntparrus', dbtable = 'ru_conll', ConditionColumns={'deprel':('огранич','аналит'), 'pos':('Q',)})
+    simpleupdate(thisSearch, dbcon, deprel='prtcl')
+
+def semsubj(dbcon=False):
+    """Otan käyttöön termin semsubj nesessiivilauseille ja muille vastaaville rakenteille, joissa SN-analyysi
+    määrittää infinitiivimuotoisen verbin subjektiksi. Näin TDT:n nsubj ja SN:n
+    2-kompl (1-kompl!) ja dat-subj muuttuvat (näiden lauseiden osalta) kontrastiivisessa
+    kerroksessa muotoon semsubj.
+    
+    Rakenteen finiittimuotoinen verbi katsotaan
+    SN-jäsennyksen mukaisesti juureksi ja infinitiivimuotoinen verbi luetaan infcomp-kategoriaan.
+    
+    Muutan kuitenkin SN-jäsennystä TDT-jäsennyksen mallin mukaiseksi siinä,
+    että semsubj-kategorialla analysoitu sanamuoto katsotaan infinitiivimuodon
+    eikä apuverbin dependentiksi. 
+    
+    HUOM!
+    - verbi on joskus muu kuin predik (1-kompl,razjasnit)
+    - semsubj-kandidaatti voi olla predik!
+    - Lisäksi infinitiivitäydennys puuttuu joskus kokonaan (мне надо к нему)
+    """
+
+    LogNewDeprel('Create the class of SemSubj in SN ')
+    featset = Featset()
+    Db.searched_table = 'ru_conll'
+    thisSearch = makeSearch(database='syntparrus',dbtable='ru_conll',  ConditionColumns={'deprel':('1-компл', '2-компл','дат-субъект')})
+    updated = 0
+    #Check out whether there is a 'predik' or infinitival 1-kompl depending on the verbal head
+    for key, matchlist in thisSearch.matches.items():
+        for match in matchlist:
+            match.matchedsentence.listDependents(match.matchedword.head)
+            for codependent in match.matchedsentence.dependentlist:
+                #If there is an infinitival codependent marked as predik or 1-kompl AND the nominal complement is in dative:
+                if (codependent.deprel in ('предик','1-компл') and codependent.feat in featset.inf) and (match.matchedword.feat in featset.NounDat or match.matchedword.feat in featset.PronDat):
+                    dbcon.query('UPDATE ru_conll SET contr_head = %(contrhead)s, contr_deprel = %(contrdep)s WHERE id = %(matchid)s',{'contrhead':codependent.tokenid,'contrdep':'semsubj','matchid':match.matchedword.dbid})
+                    updated += 1
+    log('Updated {} items in the db'.format(updated))
+
+def prdctv(dbcon=False):
+    """Koska ajanilmauksen sijainnin määrittelyn kannalta olisi hyödyllistä erottaa predikatiivit muista
+    verbin täydennyksistä, pyrin kontrastiivisessa analyysitasossa tarkentamaan SN-jäsennyksen prisvjaz-
+    kategoriaa. Tämä tapahtuu erottamalla omaksi prdctv-luokakseen sellaiset prisvjaz-kategorian sanat,
+    jotka eivät ole prepositioita. Prepositioilla ilmaistavat prisvjaz-dependentit puolestaan liitetään edellä
+    määriteltyyn gmod-kategoriaan."""
+
+    LogNewDeprel('Create the prdctv category in SN')
+    thisSearch = makeSearch(database='syntparrus', dbtable = 'ru_conll', ConditionColumns={'deprel':('присвяз',), '!pos':('S',)})
+    simpleupdate(thisSearch,dbcon,deprel='prdctv')
+
+def cdep(dbcon=False):
+    """
+    Sivulauseet SN-aineistossa:
+    
+    SN-jäsennystä muutetaan siten, että sivulauseen pääverbistä tehdään päälauseen dependentti,
+    josta sivulauseen aloittava konjunktio riippuu.
+
+    Kontrastiivinen analyysikerros toteutetaan siten, että kaikki alisteisen sivulauseen pääsanat luokitellaan
+    cdep-kategoriaan, jolloin niistä käy ilmi, että kyseessä on sivulauseen muodostama täydennys tai määrite,
+    muttei sitä, minkä tyypin argumentti on kyseessä. Kuvatunlainen kompromissi on väistämätön, sillä
+    TDT-analyysi ei anna sivulauseiden suhteesta pääsanaansa yhtä tarkkaa informaatiota kuin SN-analyysi
+    ja toisaalta SN-analyysi ei jaottele sivulauseita sen mukaan, ovatko ne täydennyksiä vai määritteitä.
+    Tuloksena on väljempi yläkäsite, josta käy ilmi se, että kyseessä on sivulauseen muodostama argumentti.
+    Tarkempi informaatio ei sinänsä katoa, sillä se on edelleen läsnä kielikohtaisissa jäsennyksissä.
+    """
+
+    LogNewDeprel('Create the cdep and the sc categories in SN')
+    thisSearch = makeSearch(database='syntparrus', dbtable = 'ru_conll', ConditionColumns={'deprel':('подч-союзн',)})
+    updated = 0
+    for key, matchlist in thisSearch.matches.items():
+        for match in matchlist:
+            try:
+                mhead = match.matchedsentence.words[match.matchedword.head]
+                dbcon.query('UPDATE ru_conll SET contr_head = %(contrhead)s, contr_deprel = %(contrdep)s WHERE id = %(matchid)s',{'contrhead':match.matchedword.tokenid,'contrdep':'sc','matchid':mhead.dbid})
+                dbcon.query('UPDATE ru_conll SET contr_head = %(contrhead)s, contr_deprel = %(contrdep)s WHERE id = %(matchid)s',{'contrhead':mhead.head,'contrdep':'cdep','matchid':match.matchedword.dbid})
+                updated += 1
+            except KeyError:
+                log('KeyError  with word {}, sentence {}'.format(match.matchedword.token,match.matchedsentence.sentence_id))
+    log('Updated {} items in the db'.format(updated))
+
+def conj(dbcon=False):
+    """Rinnasteisten elementtien osalta SN-jäsennys käyttää kolmea eri luokkaa: sotš-sojuzn, sent-sotš ja
+    sotšin.
+
+    Kontrastiivisessa analyysissa lähdetään TDT-jäsennyksen mukaisesta perusajatuksesta siinä, että
+    rinnastetun lauseen pääsana tai myöhempi rinnastettu elementti katsotaan ensimmäisen rinnastettavan
+    elementin (eikä rinnastuskonjunktion) dependentiksi. 
+    
+    - Rinnastuskonjunktiot merkitään TDT:n tapaan cc-kategorialla, 
+    - kaikki rinnastettavat elementit analysoidaan conj-kategoriaan kuuluviksi.
+
+    On vielä huomattava, että SN-jäsennyksessä sotšin-tyyppiä käytetään rinnastuskonjunktioiden lisäksi
+    luettelomuotoisissa rinnastustapauksissa myös luettelon osien kategorisointiin – TDT-analyysi luokittelee
+    vastaavat tilanteet osaksi conj-kategoriaan, osaksi erilliseen parataxis-kategoriaansa. Tässä suhteessa
+    kontrastiivista analyysia yksinkertaistetaan niin, että TDT:n parataxis-elementit samoin kuin ne venäjän
+    sotšin-tapaukset, jotka eivät ole rinnastuskonjunktioita, luokitellaan conj-kategoriaan.
+
+    MIKÄ on rinnastuskonjunktion pääsana?
+
+    сент-соч pitää joskus erikseen, koska aina ei ole sotshin
+    """
+
+    LogNewDeprel('Create the conj and cc categories in SN')
+    thisSearch = makeSearch(database='syntparrus', dbtable = 'ru_conll', ConditionColumns={'deprel':('сочин','сент-соч','соч-союзн')})
+    updated = 0
+    for key, matchlist in thisSearch.matches.items():
+        for match in matchlist:
+            if match.matchedword.pos == 'C':
+                #Update the conjunction
+                dbcon.query('UPDATE ru_conll SET  contr_deprel = %(contrdep)s WHERE id = %(matchid)s',{'contrdep':'cc','matchid':match.matchedword.dbid})
+            else:
+                if match.matchedword.head == 0 or match.matchedword.head not in match.matchedsentence.words.keys():
+                    pass
+                else:
+                    mhead = match.matchedsentence.words[match.matchedword.head]
+                    while mhead.pos == 'C' and mhead.head in match.matchedsentence.words.keys():
+                        mhead = match.matchedsentence.words[mhead.head]
+                    #Update the actual coordinated element
+                    dbcon.query('UPDATE ru_conll SET contr_head = %(contrhead)s, contr_deprel = %(contrdep)s WHERE id = %(matchid)s',{'contrhead':mhead.tokenid,'contrdep':'conj','matchid':match.matchedword.dbid})
+                    updated += 1
+    log('Updated {} items in the db'.format(updated))
+
+def attr(dbcon=False):
+    """Kaikkiin kielen tilanteisiin tarkoitettujen jäsenninten on luonnollisesti pyrittävä mahdollisimman
+    suureen tarkkuuteen myös alemman tason dependenssisuhteissa. Tämän vuoksi erilaisia substantiivia tai
+    adjektiivia määrittäviä sanoja kuvataan melko monilla alaluokilla. Näitä ovat TDT-jäsennyksessä muun
+    muassa luokat amod, det ja quantmod. SN-jäsennyksessä vastaavan tason kategorioita ovat esimerkiksi
+    opred, appos, ja utotšn.
+
+    Tämän tutkimuksen kannalta näillä suhteilla ei useinkaan ole suurta merkitystä, sillä oleellista on
+    erotella elementtejä, joiden suhteen ajanilmausten sijaintia olisi mielekästä määrittää. Tämän vuoksi
+    kontrastiiviseen analyysikerrokseen muodostuu erittäin suuri alempien dependenssitasojen luokka, jota
+    merkitään nimityksellä attr."""
+
+    lognewdeprel('create the attr category in sn')
+    thissearch = makesearch(database='syntparrus', dbtable='ru_conll', conditioncolumns={'deprel':('опред', 'квазиагент', 'атриб', 'аппоз', 'разъяснит', 'количест', 'сравн-союзн', 'сравнит', 'вспом', 'соотнос', 'колич-вспом', 'электив', 'оп-опред', 'уточн', 'колич-огран', 'аппрокс-колич', 'кратн', 'нум-аппоз', 'эллипт', 'распред', 'композ')}, headcond = {'column':'pos','values':('a','n','p')})
+    simpleupdate(thissearch,dbcon,deprel='attr')
+
+def gmod(dbcon=False):
+    """
+    Kun muut luokat analysoitu, loput verbin täydennykset ja määritteet tähän luokkaan.
+    
+    TDT- ja SN-jäsennysten erilaiset periaatteelliset lähtökohdat näkyvät siinä, että SN-jäsennin luokittelee
+    omaan obst-kategoriaansa elementit, jotka määrittävät verbiä tai muuta pääsanaa adverbiaalin tavoin.
+    Näin esimerkiksi virkkeen 29 notšju saa luokakseen obst, mutta virkkeen 30 yönä tulee määritellyksi
+    edelleen nommod-luokkaan.
+    
+    Ne prisvjaz-luokan elementit, jotka muodostetaan prepositioiden kautta, liitetään tähän.
+    """
+
+    featset = Featset()
+    lognewdeprel('Create the gmod category in sn')
+    deprels = ('1-компл', '2-компл', '3-компл', '4-компл', '5-компл','обст','присвяз')
+    thissearch = makesearch(database='syntparrus', dbtable='ru_conll', conditioncolumns={'deprel':deprels,'contr_deprel':'gdep'})
+    simpleupdate(thissearch,dbcon,deprel='gmod')
+
