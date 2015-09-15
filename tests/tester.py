1b6aca343c275d6954689ec730b0b0baa0453f04
Juho Härme <juho.harme@uta.fi>
2015-09-15 10:08:05 +0300

    Add a tester module

---
+++ tests/tester.py
@@ -0,0 +1,14 @@
+import sys
+scriptdir = '/home/juho/corpora2/syntparsedmustikka/'
+sys.path.append(scriptdir)
+import interface
+
+testmenu = interface.MainMenu()
+testmenu.selecteddb = 'syntparfin2'
+interface.Db.con = interface.mydatabase(testmenu.selecteddb,'juho')
+interface.Db.searched_table = 'fi_conll'
+testmenu.selectedlang = 'fi'
+
+#Concordancing:
+
+testmenu.Parconc()
